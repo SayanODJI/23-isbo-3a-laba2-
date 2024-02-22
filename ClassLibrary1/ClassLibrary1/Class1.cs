f846eed868b207653274c13f12ce3b7e84c3aef4
MifitoMori <ulqiorraplay15@gmail.com>
2024-02-22 19:07:17 +0300

    type of activity 13

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassLibrary1
+{
+    public class typeOfOccupation
+    {
+        private string Class = "";
+
+        public typeOfOccupation(string Class)
+        {
+            Occupation = Class;
+        }
+
+        public string Occupation
+        {
+            get { return Class; }
+            set => Class = value;
+        }
+    }
+}
