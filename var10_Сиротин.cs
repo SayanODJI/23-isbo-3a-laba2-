eac2d444a9da685874f4fae05a355d28190d7494
Никита уткин <148621095+Blupup@users.noreply.github.com>
2024-03-06 15:23:29 +0300

    Add files via upload

---
+++ var10_Сиротин.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    //Ссылка: https://docs.google.com/document/d/1ACrXX9Gcw4QhWjsbdaJLBZ5DcvxH5JZvfcD6PI8X_iY/edit?usp=sharing
+    public class Var10
+    {
+        public string Название { get; }
+        public decimal Оклад { get; }
+        public Var11Подразделение Подразделение { get; }
+        public Var10(string название, decimal оклад, Var11Подразделение подразделение)
+        {
+            Название = название;
+            Оклад = оклад;
+            Подразделение = подразделение;
+        }
+    }
+}
+
+
